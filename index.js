d03faa12da99fb5c392a52e2ff7a051378196fbe
Justin <ailton.ferrazjr@gmail.com>
2024-01-01 20:07:01 +0100

    first-git

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+
+// Load Public folder as Static
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs", {});
+})
+
+app.listen(port, (req, res) => {
+    console.log(`Server is running in port ${port}!`);
+})
